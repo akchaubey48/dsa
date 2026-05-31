8d672e06cd8f9c9b49e9bb53ded8a5a751dc75f9
Adarsh kumar chaubey <kumarchaubeyadarsh@gmail.com>
2026-05-31 23:41:51 +0530

    Add files via upload

---
+++ graph/graph.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+using namespace std;
+
+class graph{
+
+public:
+    unordered_map<int, list<int>> adj;
+
+    void add(int u, int v, bool direction){
+
+        // direction = 0 -> undirected
+        // direction = 1 -> directed
+
+        adj[u].push_back(v);
+
+        if(direction == 0){
+            adj[v].push_back(u);
+        }
+    }
+
+    void print(){
+
+        for(auto i : adj){
+
+            cout << i.first << " -> ";
+
+            for(auto j : i.second){
+                cout << j << " ";
+            }
+
+            cout << endl;
+        }
+    }
+};
+
+int main(){
+
+    int n;
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    int m;
+    cout << "Enter number of edges: ";
+    cin >> m;
+
+    graph g;
+
+    for(int i = 0; i < m; i++){
+
+        int u, v;
+        cin >> u >> v;
+
+        g.add(u, v, 0);
+    }
+
+    g.print();
+}
